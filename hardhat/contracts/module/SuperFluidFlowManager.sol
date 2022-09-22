07c6f88056fa9cb07b04743a79eeb39bdb59d990
Nartey Kodjo-Sarso <narteysarso@gmail.com>
2022-09-22 07:23:19 +0000

    Add abis

--- hardhat/contracts/module/SuperFluidFlowManager.sol
+++ hardhat/contracts/module/SuperFluidFlowManager.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.14;
+pragma solidity >0.8.14;
 
 import {ISuperfluid, ISuperToken, ISuperApp} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 import {ISuperfluidToken} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluidToken.sol";
